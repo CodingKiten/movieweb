f217de016cf3f8103137a1efceab833338199d85
CodingKiten <121097932+CodingKiten@users.noreply.github.com>
2023-12-06 18:35:08 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movieweb.1242601565.workers.dev",
+  VITE_TMDB_READ_API_KEY: "189949d4131c717b4ce5f5b5bb9ffbed"
+};
